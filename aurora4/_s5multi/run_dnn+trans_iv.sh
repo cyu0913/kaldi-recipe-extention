830140650f3d04b47436a6f1ae3e06ac702a8a3a
cyu0913 <cyu0913@gmail.com>
2016-03-07 08:44:46 -0600

    Achieved 16.07 on CDS, 0.06 on PLDA

--- aurora4/_s5multi/run_dnn+trans_iv.sh
+++ aurora4/_s5multi/run_dnn+trans_iv.sh
@@ -23,16 +23,16 @@ ivdim=200
 run_dnn_iv_extract(){
    log_start "DNN Based Ivector Extraction"
 
-   scale=0.9
+   scale=0.8
  
-   #sid/train_full_ubm_dnn+trans.sh --nj 40 --cmd "$train_cmd" data/train_si84_multi data-fbank/train_si84_multi exp/tri3a_dnn exp/full_ubm_dnn+trans exp/tri2b_multi_ali_si84 $scale || exit 1;
+   sid/train_full_ubm_dnn+trans.sh --nj 40 --cmd "$train_cmd" data/train_si84_multi data-fbank/train_si84_multi exp/tri3a_dnn exp/full_ubm_dnn+trans exp/tri2b_multi_ali_si84 $scale || exit 1;
 
-   #sid/train_ivector_extractor_dnn+trans.sh --nj 40 --cmd "$train_cmd" \
-   #     --ivector-dim $ivdim --num-iters 6 exp/full_ubm_dnn/final.ubm exp/tri3a_dnn data/train_si84_multi data-fbank/train_si84_multi \
-   #     exp/extractor_dnn+trans exp/tri2b_multi_ali_si84 $scale || exit 1;
+   sid/train_ivector_extractor_dnn+trans.sh --nj 40 --cmd "$train_cmd" \
+        --ivector-dim $ivdim --num-iters 6 exp/full_ubm_dnn+trans/final.ubm exp/tri3a_dnn data/train_si84_multi data-fbank/train_si84_multi \
+        exp/extractor_dnn+trans exp/tri2b_multi_ali_si84 $scale || exit 1;
 
    sid/extract_ivectors_dnn+trans.sh --cmd "$train_cmd" --nj 80 \
-        exp/extractor_dnn exp/tri3a_dnn data/test_eval92 data-fbank/test_eval92 data/test_eval92.dnn+trans-iv exp/tri2b_multi_ali_eval92 $scale || exit 1;
+        exp/extractor_dnn+trans exp/tri3a_dnn data/test_eval92 data-fbank/test_eval92 data/test_eval92.dnn+trans-iv exp/tri2b_multi_ali_eval92 $scale || exit 1;
 
    #sid/extract_ivectors_dnn.sh --cmd "$train_cmd" --nj 80 \
    #     exp/extractor_dnn exp/tri3a_dnn data/train_si84_multi data-fbank/train_si84_multi data/train_si84_multi.dnn-iv || exit 1;
