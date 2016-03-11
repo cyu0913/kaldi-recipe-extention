d930d1fbd24ef9444546749f0eeac782fc05e35b
cyu0913 <cyu0913@gmail.com>
2016-03-11 09:13:15 -0600

    Update

--- swbd/s5crss/run_dnn+trans_iv.sh
+++ swbd/s5crss/run_dnn+trans_iv.sh
@@ -27,15 +27,15 @@ run_dnn_iv_extract(){
  
    #sid/train_full_ubm_dnn+trans.sh --nj 40 --cmd "$train_cmd_04" data/train_nodup exp/data-fmllr-tri4/train_nodup exp/dnn5b_pretrain-dbn_dnn exp/full_ubm_dnn+trans exp/tri4_ali_nodup $scale || exit 1;
 
-   sid/train_ivector_extractor_dnn+trans.sh --nj 40 --cmd "$train_cmd_05" \
-        --ivector-dim $ivdim --num-iters 5 exp/full_ubm_dnn/final.ubm exp/dnn5b_pretrain-dbn_dnn data/train_nodup exp/data-fmllr-tri4/train_nodup \
-        exp/extractor_dnn+trans exp/tri4_ali_nodup $scale || exit 1;
+   #sid/train_ivector_extractor_dnn+trans.sh --nj 40 --cmd "$train_cmd_05" \
+   #     --ivector-dim $ivdim --num-iters 5 exp/full_ubm_dnn/final.ubm exp/dnn5b_pretrain-dbn_dnn data/train_nodup exp/data-fmllr-tri4/train_nodup \
+   #     exp/extractor_dnn+trans exp/tri4_ali_nodup $scale || exit 1;
+
+   #sid/extract_ivectors_dnn+trans.sh --cmd "$train_cmd_05" --nj 40 \
+   #     exp/extractor_dnn+trans exp/dnn5b_pretrain-dbn_dnn data/eval2000 exp/data-fmllr-tri4/eval2000 data/eval2000.dnn+trans-iv exp/tri4_ali_eval2000 $scale || exit 1;
 
    sid/extract_ivectors_dnn+trans.sh --cmd "$train_cmd_05" --nj 40 \
-        exp/extractor_dnn+trans exp/dnn5b_pretrain-dbn_dnn data/eval2000 exp/data-fmllr-tri4/eval2000 data/eval200.dnn+trans-iv exp/tri4_ali_nodup $scale || exit 1;
-
-   sid/extract_ivectors_dnn+trans.sh --cmd "$train_cmd_05" --nj 80 \
-        exp/extractor_dnn+trans exp/dnn5b_pretrain-dbn_dnn data/train_nodup exp/data-fmllr-tri4/train_nodup data/train_nodup.dnn-iv || exit 1;
+        exp/extractor_dnn+trans exp/dnn5b_pretrain-dbn_dnn data/train_nodup exp/data-fmllr-tri4/train_nodup data/train_nodup.dnn+trans-iv exp/tri4_ali_nodup $scale || exit 1;
 
    log_end "DNN Based Ivector Extraction"
 }
